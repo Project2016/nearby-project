103426efb4340ab5a70440ad5e784c80882f10bb
fatimah <umhadifatimah@hotmail.com>
2016-02-09 15:17:09 -0800

    adding home page

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+
+<div id="container">
+<div id="content">
+<div id="contentleft">
+<h1>Welcome to NearBy Shops</h1>
+<p>Welcome to NearBy Shops </p>
+</div><!--contentleft-->
+
+<?php include('sidebar.php'); ?>
+
+</div><!--content-->
+</div><!--container-->
+
+<?php include('footer.php'); ?>
