85518f39ab368ffdf650f1dcefdd77112956360f
fatimah <umhadifatimah@hotmail.com>
2016-02-10 22:00:04 -0800

    modified footer.php file

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 <ul>
-		<li><a href="index.php">Home</a></li>
+		<li><a href="home.php">Home</a></li>
 		<li><a href="about.php">About</a></li>
 		<li><a href="contact.php">Contact</a></li>
 	</ul>
