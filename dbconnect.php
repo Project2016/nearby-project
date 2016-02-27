740194019c6974ce0603ce57fc56225aeb9eba17
Raveena08 <raveena.hbad@gmail.com>
2016-02-26 22:52:58 -0800

    MYSQL CODE
    The main code

--- dbconnect.php
+++ dbconnect.php
@@ -51,6 +51,68 @@ mysqli_close($con);
 ?> 
 */
 
+// this is MYSQL code for database 
+/*
+
+create table student(
+	name varchar(50) NOT NULL,
+	mail varchar(50),
+	phone varchar(20),
+	password varchar(20) NOT NULL,
+	address varchar(100) NOT NULL,
+	location_lat float,
+	location_lng float,
+	PRIMARY KEY (mail)
+);
+
+create table shop(
+	shopid int AUTO_INCREMENT,
+	shopname varchar(50) NOT NULL,
+	phone varchar(20),
+	address varchar(100) NOT NULL,
+	location_lat float,
+	location_lng float,
+	PRIMARY KEY (shopid)
+);
+
+create table category(
+	cateid int AUTO_INCREMENT,
+	catename varchar(30),
+	PRIMARY KEY (cateid)
+);
+
+create table shop_category(
+	shop int references shop(shopid),
+	category int references category(cateid),
+	PRIMARY KEY (shop,category)	
+);
+
+//TWo more fields are added 
+
+
+*/
+/*
+
+CREATE TABLE `category` (
+  `cateid` int(11) NOT NULL,
+  `catename` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `shop` (
+  `shopid` int(11) NOT NULL,
+  `shopname` varchar(50) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(100) NOT NULL,
+  `location_lat` float DEFAULT NULL,
+  `location_lng` float DEFAULT NULL,
+  `catename` varchar(30) DEFAULT NULL,
+  `zipcode` int(5) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+
+*/
+
 
 ?>
 
