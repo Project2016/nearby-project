be63ecd307419e21bc6de3c12fb98915d1a9d382
Raveena08 <raveena.hbad@gmail.com>
2016-02-26 22:47:29 -0800

    Alternate database code
    it is commented out.
    in case of help

--- dbconnect.php
+++ dbconnect.php
@@ -7,4 +7,50 @@ if(!mysql_select_db("dbtest"))
 {
      die('oops database selection problem ! --> '.mysql_error());
 }
+
+// alternate PHP DB CODE. RECOMMEND THIS 
+/*<?php
+// create database 
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "***";//check your own databse 
+$dbname = "ShopNearMe"; // the name should be same 
+$connection =mysqli_connect($dbhost, $dbuser,$dbpass, $dbname);
+
+//$con=mysqli_connect("localhost","root","Yes");
+//mysqli_select_db("ShopNearMe",$con);
+
+
+// test my database connection 
+
+if(mysqli_connect_errno())
+{
+die("datbase connection fail :" .
+mysqli_connect_error() .
+" ( " . mysqli_connect_errno() . " ) "
+);
+
+
+}
 ?>
+
+
+<html lang="en">
+	<head>
+		<title>databaser</title>
+	</head>
+	<body>
+hgyjhbjhbhj
+	</body>
+</html>
+
+<?php 
+//close databse 
+mysqli_close($con);
+
+?> 
+*/
+
+
+?>
+
