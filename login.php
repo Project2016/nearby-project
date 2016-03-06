589fb6a07e30334732b010a7afaba2599aa9ce83
Ahmad Alsafwani <Ahmad Alsafwani>
2016-03-05 20:50:13 -0800

    Fatimah : editing login file

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ include_once 'dbconnect.php';
 
 if(isset($_SESSION['user'])!="")
 {
- header("Location: home.php");
+ header("Location: Search.php");
 }
 if(isset($_POST['btn-login']))
 {
@@ -31,8 +31,8 @@ if(isset($_POST['btn-login']))
  
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE >
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>cleartuts - Login & Registration System</title>
