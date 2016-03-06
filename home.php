5230c7b6fff35c6ff8d1ba849f2b6c8c9cd09c4e
Ahmad Alsafwani <Ahmad Alsafwani>
2016-03-05 20:40:08 -0800

    adding picture to home.php file

--- home.php
+++ home.php
@@ -6,7 +6,9 @@
 <h1>Welcome to NearBy Shops</h1>
 <p>Welcome to NearBy Shops </p>
 <p>This is a project to make website that help anyone to find information about nearby shops, malls ,or markets. </p>
-<a href=""><img src="Under_Construction.jpg"/></a>
+
+<p><a href=""><img src="homepic.jpg"width="400" height="400" align="middle"/></a></p>
+
 </div><!--contentleft-->
 
 <?php include('sidebar.php'); ?>
