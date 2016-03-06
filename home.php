6cf55c71c8250870db0eea4c197ab30e2e47a65d
Ahmad Alsafwani <Ahmad Alsafwani>
2016-03-05 21:19:03 -0800

    Fatimah: editing the home page

--- home.php
+++ home.php
@@ -6,6 +6,7 @@
 <h1>Welcome to NearBy Shops</h1>
 <p>Welcome to NearBy Shops </p>
 <p>This is a project to make website that help anyone to find information about nearby shops, malls ,or markets. </p>
+<p> In order to get benefit from Nearby shops website you need to Sign up </p>
 
 <p><a href=""><img src="homepic.jpg"width="400" height="400" align="middle"/></a></p>
 
